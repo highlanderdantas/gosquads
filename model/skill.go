9aeb1e2050ca807baa7c51fcd0d39f6392e47f4a
Highlander Dantas <haylander60@gmail.com>
2019-09-02 13:49:39 -0300

    save.

--- model/skill.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "gopkg.in/mgo.v2/bson"
-
-//Skill habilidade de um usuario
-type Skill struct {
-	ID          bson.ObjectId `bson:"_id" json:"id"`
-	Description string        `bson:"description" json:"description"`
-}
